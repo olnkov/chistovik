9c11597065ddb54eb25a36ab83ac711b0fbe1521
olnkov <oleynikovsasha2006@gmail.com>
2026-03-08 15:53:00 +1000

    добавлен файл templates.cpp

---
+++ lab5/templates.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <windows.h>
+
+using namespace std;\
+
+ void contactfinder() {
+	cout << "найденные контакты:";
+}
